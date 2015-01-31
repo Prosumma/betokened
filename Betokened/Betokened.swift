5d7ef0a2cf5588115ca1da28c3e719f8e9970ff5
Gregory Higley <code@revolucent.net>
2015-01-31 16:40:58 -0500

    Added | operator

--- Betokened/Betokened.swift
+++ Betokened/Betokened.swift
@@ -78,11 +78,14 @@ public class StringStream: StringLiteralConvertible {
     }
 }
 
-public class Tokenizer<T> {
+public final class Tokenizer<T> {
     typealias Recognizer = StringStream -> RecognizerResult<T>?
     private let recognizers: [Recognizer]
     public init(_ recognizers: [Recognizer]) {
         self.recognizers = recognizers
+    }
+    public convenience init(_ recognizers: Recognizer...) {
+        self.init(recognizers)
     }
     public func tokenize(stream: StringStream) -> TokenizerResult<T> {
         var tokenizerResult: TokenizerResult<T>?
@@ -130,6 +133,38 @@ public func >> <T>(parseResult: ParserResult?, tokenize: (String, Range<String.I
 
 public func >> <T>(parse: StringStream -> ParserResult?, tokenize: (String, Range<String.Index>) -> T)(stream: StringStream) -> RecognizerResult<T>? {
     return transform(parse, tokenize)(stream: stream)
+}
+
+public func combine<T>(lrecognizer: StringStream -> RecognizerResult<T>?, rrecognizer: StringStream -> RecognizerResult<T>?) -> Tokenizer<T> {
+    return Tokenizer<T>(lrecognizer, rrecognizer)
+}
+
+public func combine<T>(ltokenizer: Tokenizer<T>, rtokenizer: Tokenizer<T>) -> Tokenizer<T> {
+    return Tokenizer<T>(ltokenizer.recognizers + rtokenizer.recognizers)
+}
+
+public func combine<T>(recognizer: StringStream -> RecognizerResult<T>?, tokenizer: Tokenizer<T>) -> Tokenizer<T> {
+    return combine(Tokenizer<T>(recognizer), tokenizer)
+}
+
+public func combine<T>(tokenizer: Tokenizer<T>, recognizer: StringStream -> RecognizerResult<T>?) -> Tokenizer<T> {
+    return combine(tokenizer, Tokenizer<T>(recognizer))
+}
+
+public func | <T>(lrecognizer: StringStream -> RecognizerResult<T>?, rrecognizer: StringStream -> RecognizerResult<T>?) -> Tokenizer<T> {
+    return combine(lrecognizer, rrecognizer)
+}
+
+public func | <T>(ltokenizer: Tokenizer<T>, rtokenizer: Tokenizer<T>) -> Tokenizer<T> {
+    return combine(ltokenizer, rtokenizer)
+}
+
+public func | <T>(recognizer: StringStream -> RecognizerResult<T>?, tokenizer: Tokenizer<T>) -> Tokenizer<T> {
+    return combine(recognizer, tokenizer)
+}
+
+public func | <T>(tokenizer: Tokenizer<T>, recognizer: StringStream -> RecognizerResult<T>?) -> Tokenizer<T> {
+    return combine(tokenizer, recognizer)
 }
 
 public func whitespace(stream: StringStream) -> ParserResult? {
