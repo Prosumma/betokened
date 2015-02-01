dfd2559a56e4d54ffa3ecb363cde9a8edfedd073
Gregory Higley <code@revolucent.net>
2015-01-31 20:37:55 -0500

    Added thunks to Parsers.
    Not sure it's strictly necessary, but I had ARC problems in Parsimonious
    when I didn't do this.

--- Betokened/Parsers.swift
+++ Betokened/Parsers.swift
@@ -10,44 +10,50 @@ import Foundation
 import Prosumma
 
 public func string(match: String)(stream: StringStream) -> ParserResult? {
-    var parserResult: ParserResult?
-    if !stream.atEnd {
-        let string = stream.string
-        if let range = string.rangeOfString(match, options: nil, range: string.startIndex..<string.endIndex, locale: nil) {
-            if range.startIndex == string.startIndex {
-                let streamRange = stream.convert(range)
-                parserResult = .Ok(match, streamRange)
-                stream.index = advance(streamRange.endIndex, 1)
+    return {
+        var parserResult: ParserResult?
+        if !stream.atEnd {
+            let string = stream.string
+            if let range = string.rangeOfString(match, options: nil, range: string.startIndex..<string.endIndex, locale: nil) {
+                if range.startIndex == string.startIndex {
+                    let streamRange = stream.convert(range)
+                    parserResult = .Ok(match, streamRange)
+                    stream.index = advance(streamRange.endIndex, 1)
+                }
             }
         }
-    }
-    return parserResult
+        return parserResult
+    }()
 }
 
 public func oneCharOf(string: String)(stream: StringStream) -> ParserResult? {
-    var parserResult: ParserResult?
-    if let c = first(stream.string) {
-        for ch in string {
-            if c == ch {
-                parserResult = .Ok(String(c), stream.index..<advance(stream.index, 1))
-                stream.index = advance(stream.index, 1)
-                break
+    return {
+        var parserResult: ParserResult?
+        if let c = first(stream.string) {
+            for ch in string {
+                if c == ch {
+                    parserResult = .Ok(String(c), stream.index..<advance(stream.index, 1))
+                    stream.index = advance(stream.index, 1)
+                    break
+                }
             }
         }
-    }
-    return parserResult
+        return parserResult
+    }()
 }
 
 public func oneStringOf(strings: [String])(stream: StringStream) -> ParserResult? {
-    var parserResult: ParserResult?
-    for s in strings {
-        parserResult = string(s)(stream: stream)
-        if parserResult != nil { break }
-    }
-    return parserResult
+    return {
+        var parserResult: ParserResult?
+        for s in strings {
+            parserResult = string(s)(stream: stream)
+            if parserResult != nil { break }
+        }
+        return parserResult
+    }()
 }
 
 public func oneStringOf(strings: String...)(stream: StringStream) -> ParserResult? {
-    return oneStringOf(strings)(stream: stream)
+    return { oneStringOf(strings)(stream: stream) }()
 }
 
--- BetokenedTests/BetokenedTests.swift
+++ BetokenedTests/BetokenedTests.swift
@@ -9,11 +9,26 @@
 import UIKit
 import XCTest
 
-enum Token {
+enum Token: DebugPrintable {
     case Literal(String)
     case Op(Character)
+    var debugDescription: String {
+        switch self {
+        case .Literal(let s): return "Literal '\(s)'"
+        case .Op(let c): return "Operator \(c)"
+        }
+    }
 }
 
 class BetokenedTests: XCTestCase {
+    
+    func testOneCharOf() {
+        let op = oneCharOf("(+)") >> { string, _ in Token.Op(Character(string)) }
+        let tokenizer = whitespace | op
+        switch tokenizer.tokenize("  (") {
+        case .Ok(let tokens): println(tokens)
+        case .Err(let error): println(error)
+        }
+    }
     
 }
