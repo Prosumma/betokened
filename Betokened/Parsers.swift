2a6218e78cfe2e1ee70db2e50f148e73e6395b87
Gregory Higley <code@revolucent.net>
2015-02-01 08:16:12 -0500

    Added regex and delimit.
    Delimit is currently broken.

--- Betokened/Parsers.swift
+++ Betokened/Parsers.swift
@@ -18,7 +18,7 @@ public func string(match: String)(stream: StringStream) -> ParserResult? {
                 if range.startIndex == string.startIndex {
                     let streamRange = stream.convert(range)
                     parserResult = .Ok(match, streamRange)
-                    stream.index = advance(streamRange.endIndex, 1)
+                    stream.index = streamRange.endIndex
                 }
             }
         }
@@ -57,3 +57,69 @@ public func oneStringOf(strings: String...)(stream: StringStream) -> ParserResul
     return { oneStringOf(strings)(stream: stream) }()
 }
 
+public func regex(pattern: String)(stream: StringStream) -> ParserResult? {
+    return {
+        var parserResult: ParserResult?
+        var error = NSErrorPointer()
+        let string = stream.string
+        if let range = string.rangeOfString(pattern, options: .RegularExpressionSearch, range: string.startIndex..<string.endIndex, locale: nil) {
+            if range.startIndex == string.startIndex {
+                let streamRange = stream.convert(range)
+                parserResult = .Ok(string.substringWithRange(range), streamRange)
+                stream.index = streamRange.endIndex
+            }
+        }
+        return parserResult
+    }()
+}
+
+public func delimit(start: Character, end: Character, escape: Character? = nil)(stream: StringStream) -> ParserResult? {
+    return {
+        var parserResult: ParserResult?
+        let string = stream.string
+        let startIndex = string.startIndex
+        if let c = first(string) {
+            if start == c {
+                var parsed = ""
+                var escaping = false
+                var index = startIndex
+                while true {
+                    index = advance(index, 1)
+                    if index == string.endIndex {
+                        parserResult = .Err(Error.UnterminatedDelimiter(stream.index))
+                        break
+                    }
+                    var c = string[index]
+                    if escaping {
+                        if c == end {
+                            parsed.append(c)
+                        } else if end == escape {
+                            let streamRange = stream.convert(startIndex..<index)
+                            parserResult = .Ok(parsed, streamRange)
+                            stream.index = streamRange.endIndex
+                            break
+                        } else {
+                            parsed.append(escape!)
+                            parsed.append(c)
+                        }
+                        escaping = false
+                    } else if c == escape {
+                        escaping = true
+                    } else if c == end {
+                        let streamRange = stream.convert(startIndex..<index)
+                        parserResult = .Ok(parsed, streamRange)
+                        stream.index = advance(streamRange.endIndex, 1)
+                        break
+                    } else {
+                        parsed.append(c)
+                    }
+                }
+            }
+        }
+        return parserResult
+    }()
+}
+
+public func quote(mark: Character, escape: Character? = nil)(stream: StringStream) -> ParserResult? {
+    return delimit(mark, mark, escape: mark)(stream: stream)
+}
--- BetokenedTests/BetokenedTests.swift
+++ BetokenedTests/BetokenedTests.swift
@@ -8,27 +8,108 @@
 
 import UIKit
 import XCTest
+import Swift
 
-enum Token: DebugPrintable {
+enum Token: DebugPrintable, Equatable {
     case Literal(String)
     case Op(Character)
     var debugDescription: String {
         switch self {
-        case .Literal(let s): return "Literal '\(s)'"
+        case .Literal(let s): return "Literal \(s)"
         case .Op(let c): return "Operator \(c)"
         }
     }
 }
 
+func == (lhs: Token, rhs: Token) -> Bool {
+    switch lhs {
+    case .Literal(let ls):
+        switch rhs {
+        case .Literal(let rs): return ls == rs
+        default: false
+        }
+    case .Op(let lop):
+        switch rhs {
+        case .Op(let rop): return lop == rop
+        default: false
+        }
+    }
+    return false
+}
+
+let TokenMatchFailed = "Token match failed."
+
 class BetokenedTests: XCTestCase {
+    
+    func testDelimitCurlyBracesWithoutEscape() {
+        let braces = delimit("{", "}", escape: nil) >> { string, _ in Token.Literal(string) }
+        let parens = oneCharOf("()") >> { string, _ in Token.Op(Character(string)) }
+        let tokenizer = whitespace | parens | braces | end
+        switch tokenizer.tokenize("(   {betokened}    )") {
+        case .Ok(let tokens):
+            XCTAssertEqual(tokens[1], Token.Literal("betokened"), TokenMatchFailed)
+            println(tokens)
+        case .Err(let error): XCTFail(TokenMatchFailed)
+        }
+    }
+    
+    /*
+    func testDelimitCurlyBracesWithEscape() {
+        let braces = delimit("{", "}", escape: "^") >> { string, _ in Token.Literal(string) }
+        let parens = oneCharOf("()") >> { string, _ in Token.Op(Character(string)) }
+        let tokenizer = whitespace | parens | braces | end
+        switch tokenizer.tokenize("(   {be^^toke^}ned}    )") {
+        case .Ok(let tokens):
+            XCTAssertEqual(tokens[1], Token.Literal("be^^toke}ned"), TokenMatchFailed)
+            println(tokens)
+        case .Err(let error): XCTFail(TokenMatchFailed)
+        }
+    }
+    */
+    
+    func testQuotesWithoutEscape() {
+        let quotes = quote("'", escape: nil) >> { string, _ in Token.Literal(string) }
+        let tokenizer = whitespace | quotes | end
+        switch tokenizer.tokenize(" 'betokened'") {
+        case .Ok(let tokens):
+            XCTAssertEqual(tokens[0], Token.Literal("betokened"), TokenMatchFailed)
+            println(tokens)
+        case .Err(let error): XCTFail(TokenMatchFailed)
+        }
+    }
+    
+    func testString() {
+        let s = string("betokened") >> { string, _ in Token.Literal(string) }
+        let op = oneCharOf("(+)") >> { string, _ in Token.Op(Character(string)) }
+        let tokenizer = whitespace | op | s
+        switch tokenizer.tokenize("( betokened)") {
+        case .Ok(let tokens):
+            XCTAssertEqual(tokens[1], Token.Literal("betokened"), TokenMatchFailed)
+            println(tokens)
+        case .Err(let error): XCTFail(TokenMatchFailed)
+        }
+    }
     
     func testOneCharOf() {
         let op = oneCharOf("(+)") >> { string, _ in Token.Op(Character(string)) }
         let tokenizer = whitespace | op
         switch tokenizer.tokenize("  (") {
-        case .Ok(let tokens): println(tokens)
-        case .Err(let error): println(error)
+        case .Ok(let tokens):
+            XCTAssertEqual(tokens.first!, Token.Op("("), TokenMatchFailed)
+            println(tokens)
+        case .Err(let error): XCTFail(TokenMatchFailed)
         }
     }
-    
+
+    func testRegex() {
+        let r = regex("[^\\s+()]+") >> { string, _ in Token.Literal(string) }
+        let op = oneCharOf("(+)") >> { string, _ in Token.Op(Character(string)) }
+        let tokenizer = whitespace | op | r | end
+        switch tokenizer.tokenize("( betokened)   ") {
+        case .Ok(let tokens):
+            XCTAssertEqual(tokens[1], Token.Literal("betokened"), TokenMatchFailed)
+            println(tokens)
+        case .Err(let error): XCTFail(TokenMatchFailed)
+        }
+    }
 }
